36601a4892b3ddb8668fa403a75ac0188d737ca5
nrao57 <raonikhil57@gmail.com>
2015-03-11 13:57:01 -0400

    first commit

---
+++ src/testHands/TestHands.java
@@ -0,0 +1,82 @@
+package testHands;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import poker.Card;
+import poker.Deck;
+import poker.Hand;
+import poker.eHandStrength;
+import poker.eRank;
+import poker.eSuit;
+
+public class TestHands {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void TestRoyalFlush1() {
+
+		ArrayList<Card> rfHand = new ArrayList<Card>();
+		rfHand.add(new Card(eSuit.CLUBS,eRank.TEN));
+		rfHand.add(new Card(eSuit.CLUBS,eRank.JACK));
+		rfHand.add(new Card(eSuit.CLUBS,eRank.QUEEN));
+		rfHand.add(new Card(eSuit.CLUBS,eRank.KING));
+		rfHand.add(new Card(eSuit.CLUBS,eRank.ACE));
+		Hand h = Hand.EvalHand(rfHand);
+		
+		assertEquals("Should be rf:",eHandStrength.RoyalFlush.getHandStrength(),h.getHandStrength());
+		
+	}
+
+	@Test
+	public final void TestRoyalFlush2() {
+		
+		ArrayList<Card> rfHand = new ArrayList<Card>();
+		rfHand.add(new Card(eSuit.CLUBS,eRank.KING));
+		rfHand.add(new Card(eSuit.CLUBS,eRank.ACE));
+		rfHand.add(new Card(eSuit.CLUBS,eRank.TEN));
+		rfHand.add(new Card(eSuit.CLUBS,eRank.JACK));
+		rfHand.add(new Card(eSuit.CLUBS,eRank.QUEEN));
+		Hand h = Hand.EvalHand(rfHand);		
+		
+		assertEquals("Should be rf:",eHandStrength.RoyalFlush.getHandStrength(),h.getHandStrength());		
+	}
+	
+	@Test
+	public final void TestStraightFlush1() {
+		
+		ArrayList<Card> rfHand = new ArrayList<Card>();
+		rfHand.add(new Card(eSuit.CLUBS,eRank.KING));
+		rfHand.add(new Card(eSuit.CLUBS,eRank.TEN));
+		rfHand.add(new Card(eSuit.CLUBS,eRank.NINE));
+		rfHand.add(new Card(eSuit.CLUBS,eRank.JACK));
+		rfHand.add(new Card(eSuit.CLUBS,eRank.QUEEN));
+		Hand h = Hand.EvalHand(rfHand);		
+		
+		assertEquals("Should be Sf:",eHandStrength.StraightFlush.getHandStrength(),h.getHandStrength());		
+	}
+
+
+}
